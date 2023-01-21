5a54294d2853d4e85902b4c1eb40234fbe585e72
ramesh <ramesh.sharma2056@gmail.com>
2023-01-21 16:37:45 +0530

    Implemented movie service using flatMap operator.

---
+++ src/test/java/com/learnreactiveprogramming/service/MovieReactiveServiceTest.java
@@ -0,0 +1,31 @@
+package com.learnreactiveprogramming.service;
+
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieReactiveServiceTest {
+
+    MovieInfoService movieInfoService = new MovieInfoService();
+    ReviewService reviewService = new ReviewService();
+    MovieReactiveService movieReactiveService = new MovieReactiveService(movieInfoService, reviewService);
+    @Test
+    void getAllMovies() {
+
+        StepVerifier.create(movieReactiveService.getAllMovies())
+                .assertNext(movie -> {
+                    assertEquals("Batman Begins", movie.getMovie().getName());
+                    assertEquals(2, movie.getReviewList().size());
+                })
+                .assertNext(movie -> {
+                    assertEquals("The Dark Knight", movie.getMovie().getName());
+                    assertEquals(2, movie.getReviewList().size());
+                })
+                .assertNext(movie -> {
+                    assertEquals("Dark Knight Rises", movie.getMovie().getName());
+                    assertEquals(2, movie.getReviewList().size());
+                })
+                .verifyComplete();
+    }
+}
