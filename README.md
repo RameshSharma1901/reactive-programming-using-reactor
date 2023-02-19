630a74de3f78943d153c21a4f8593dd25bd9cdf2
RameshSharma1901 <44294869+RameshSharma1901@users.noreply.github.com>
2023-02-19 01:00:34 +0000

    Added example for Flux.generate

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # reactive-programming-using-reactor
 This project has the necessary code for the reactive programming course using Project Reactor
 
+## Flux.create and Flux.generate difference and examples
+https://www.vinsguru.com/reactor-flux-create-vs-generate/
+ 
 ## Swagger-UI
 
 -   Check the following [link](http://localhost:8080/movies/swagger-ui.html) for swagger.
--- src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -8,5 +8,16 @@ public class FunctionalExample {
     public static void main(String[] args) {
         Flux.fromIterable(List.of("ramesh", "Sharma")).log().subscribe(System.out::println);
 
+        // Example for Flux.create()
+        Flux.generate(()->0, (state, sink)->{
+            state = state + 1;
+            sink.next(3*state);
+            System.out.println("3 x "+state+" = "+3*state);
+            if (state == 10)
+                sink.complete();
+            return state;
+        }).log().subscribe();
+
+        // Example for Flux.generate
     }
 }
