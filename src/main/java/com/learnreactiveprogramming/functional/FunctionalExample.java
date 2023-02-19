5f780acd5ad84cfbaf09ddef155c5d7abe3b8c0f
ramesh <ramesh.sharma2056@gmail.com>
2023-02-19 16:21:10 +0530

    Added Example for Flux.create()

--- src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -1,14 +1,45 @@
 package com.learnreactiveprogramming.functional;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.util.function.Tuples;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class FunctionalExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Flux.fromIterable(List.of("ramesh", "Sharma")).log().subscribe(System.out::println);
 
-        // Example for Flux.create()
+        fluxGenerateTableExample();
+
+        fluxGenerateFibonacciSeriesExample();
+
+        fluxCreateExample();
+
+    }
+
+    private static void fluxCreateExample() {
+        Flux<Integer> sequence = Flux.create((FluxSink<Integer> fluxSink) -> {
+            IntStream.range(1,10)
+                    .peek(i-> System.out.println("Emitting element : "+ i))
+                    .forEach(fluxSink::next);
+        }).log();
+
+        sequence.delayElements(Duration.ofSeconds(2)).subscribe(i ->
+                System.out.println("Received :"+i));
+    }
+
+    private static void fluxGenerateFibonacciSeriesExample() {
+        Flux.generate( () -> Tuples.of(0, 1), (state, sink) -> {
+            sink.next(state.getT1());
+            System.out.println(state.getT1());
+            return Tuples.of(state.getT2(), state.getT1() + state.getT2());
+        }).take(5).log().subscribe();
+    }
+
+    private static void fluxGenerateTableExample() {
         Flux.generate(()->0, (state, sink)->{
             state = state + 1;
             sink.next(3*state);
@@ -17,7 +48,5 @@ public class FunctionalExample {
                 sink.complete();
             return state;
         }).log().subscribe();
-
-        // Example for Flux.generate
     }
 }
